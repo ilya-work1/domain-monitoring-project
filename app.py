8f3e74cb2a82098567f7c991a2888d00c39b9db4
RazielRey <raziel.cohen@gmail.com>
2024-11-14 13:25:37 +0200

    added changes to local flask

--- app.py
+++ app.py
@@ -7,9 +7,18 @@ app = Flask(__name__)
 def file(filename):
     return app.send_static_file(filename)
 
+def load_user_domains(username):
+    with open(f"{username}_domains.txt", 'r') as d:
+        data = json.load(d)
+    return data.get('domains')
+
+
+
 @app.route('/')
 def index():   
     return render_template('index.html')
+
+
 
 # Run the Flask app
 if __name__ == '__main__':
