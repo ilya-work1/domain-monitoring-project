c06472206f87f04d99b38ca393e7ea187e168c47
Matanelle <ematan99@gmail.com>
2024-11-17 22:59:06 +0200

    Added Registration functionallity

--- app.py
+++ app.py
@@ -55,6 +55,13 @@ def login():
 def register():
     return render_template('registration.html')
 
+@app.route('/NewUser', methods=['POST'])
+def NewUser():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    registration(username, password)
+    return redirect('/')
+    
 
 @app.route("/logout")
 def logout():
